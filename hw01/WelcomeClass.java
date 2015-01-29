9e70281bc38fd15e0b128cabfeaf4a715b09548e
Gabriel Gaydos <gabo1196@yahoo.com>
2015-01-29 22:35:34 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+// Gabriel Gaydos
+// Hw01
+
+public class WelcomeClass {
+    public static void main(String[ ] args) {
+        System.out.println("   -----------   ");
+        System.out.println("   | WELCOME |   ");
+        System.out.println("   -----------   ");
+        System.out.println("   ^ ^ ^ ^ ^ ^   ");
+        System.out.println("   / \\/ \\/ \\/ \\/ \\  ");
+        System.out.println("   <-G--M--G--2--1--8->   ");
+        System.out.println("   \\ /\\ /\\ /\\ /\\ /  ");
+        System.out.println("   v  v  v  v  v  v   ");
+        System.out.println("Last summer I flew a private jet to Contadora in Panama");
+        System.out.println("and stayed in the house of the Bacardi owners");
+    
+    }
+}
